45b39f1a3645e8ca5267a422f2e0ca6724ae6b73
Jenny Wang <sincerelyjenny@Andromeda.local>
2014-01-25 14:52:37 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsGirlsLinkedIn::Application.config.secret_key_base = '82f7df5a24b2bc05e2642816e4e9b17c608ccf5e7edf42cffbab4eff92b749d535785540b1de8523b23daf75ec2f34409bb6562db3e85651f05b7d57d723b341'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsGirlsLinkedIn::Application.config.session_store :cookie_store, key: '_RailsGirlsLinkedIn_session'
